c6141cc0d37a05ce3c922a4896950dbc45219f2e
bramvonk <github@bramvonk.nl>
2016-03-31 20:06:00 +0200

    Code layout

--- src/joystick.py
+++ src/joystick.py
@@ -11,8 +11,7 @@ class Joystick:
         self.joystick = pygame.joystick.Joystick(0)
         self.joystick.init()
 
-    def wait_for_button_press(self, timeout_in_seconds = -1):
-
+    def wait_for_button_press(self, timeout_in_seconds=-1):
         while timeout_in_seconds == -1 or timeout_in_seconds > 0:
             if self.is_button_currently_pressed():
                 # first wait until no buttons are pressed anymore (to prevent double presses)
